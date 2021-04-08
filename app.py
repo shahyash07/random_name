0570abe2f6c91890adbbeffb4f9d2ea15fecf9fb
shahyash07 <65792712+shahyash07@users.noreply.github.com>
2021-04-08 15:18:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import streamlit as st
 import numpy as np
 import random
 import pandas as pd
-name =  ['Alex','Anton','Louise','Amandeep','Brenda','Ganesh','Dmytry','Kiren','Yuliya','Robbie','Yash', 'Hiba', 'Michael', 'Pamela'] 
+
 
 def run():
+  
   name = ['Alex','Anton','Louise','Amandeep','Brenda','Ganesh','Dmytry','Kiren','Yuliya','Robbie','Yash', 'Hiba', 'Michael', 'Pamela'] 
+  q_list = ['Who was your childhood actor/actress crush?', 'What is your favorite item you’ve bought this year?', 'What would your dream house be like?',
+          'What is your favorite breakfast food?', 'which was your favorite trip', 'If you could live anywhere in the world for a year, where would it be?',
+           'What is the best gift ever you ever received?']
+
+
+
   from PIL import Image
   image = Image.open('logo.PNG')
   image2 = Image.open('logo3.jpg')
@@ -23,17 +30,25 @@ def run():
 
   # if len(name)== 0:
   #   st.success('all names have been selected')
-  col1, col2 = st.beta_columns(2) 
-  with col1:
+  col1, col2, col3 = st.beta_columns(3) 
+  with col3:
     if st.button("Who's the Host"):
       new_li = random.choice(name)
       name = [n for n in name if n !=new_li]
       st.success(new_li)
-  with col2: 
+  with col1: 
     if st.button("Choose Order"):
       new_list = random.sample(name,len(name))
       new_list = pd.DataFrame(new_list, columns={'Order'})
       st.dataframe(new_list,width=10000, height=10000)
+  with col2:
+    if st.button("Question?"):
+      new_qu = random.choice(q_list)
+      st.success(new_qu)
+
+
+
+
       # name.remove(output)
   # name.remove(output)
 
